4eb13861249d57347d6f68e1f3f0fa1d78c3a88e
Stoica <stoicatedy@gmail.com>
2019-07-31 14:28:22 +0300

    made the linter to stop complaining

--- storage.go
+++ storage.go
@@ -13,11 +13,12 @@ import (
 
 var db *sql.DB
 
-//User is the user type
+// User is needed for json reply when logged in.
 type User struct {
 	Name string `json:"name"`
 }
 
+// Authenticator is needed for webauthn protocol
 type Authenticator struct {
 	ID           []byte
 	CredentialID []byte
@@ -26,11 +27,13 @@ type Authenticator struct {
 	SignCount    uint32
 }
 
+// Storage is needed for webauthn protocol
 type Storage struct {
 }
 
 var storage = &Storage{}
 
+// AddAuthenticator is needed for webauthn protocol
 func (s *Storage) AddAuthenticator(user webauthn.User, authenticator webauthn.Authenticator) error {
 	stmt, err := db.Prepare("INSERT INTO authenticators(User, ID, CredentialID, PublicKey, AAGUID, SignCount) VALUES (?, ?, ?, ?, ?, ?)")
 	if err != nil {
@@ -51,6 +54,7 @@ func (s *Storage) AddAuthenticator(user webauthn.User, authenticator webauthn.Au
 	return nil
 }
 
+// GetAuthenticator is needed for webauthn protocol
 func (s *Storage) GetAuthenticator(id []byte) (webauthn.Authenticator, error) {
 	var au Authenticator
 	stmt, err := db.Prepare("SELECT ID,CredentialID,PublicKey,AAGUID,SignCount FROM authenticators WHERE ID = ?")
@@ -74,6 +78,7 @@ func (s *Storage) GetAuthenticator(id []byte) (webauthn.Authenticator, error) {
 	return nil, fmt.Errorf("authenticator not found")
 }
 
+// GetAuthenticators is needed for webauthn protocol
 func (s *Storage) GetAuthenticators(user webauthn.User) ([]webauthn.Authenticator, error) {
 	var authrs []webauthn.Authenticator
 	stmt, err := db.Prepare("SELECT ID, CredentialID, PublicKey, AAGUID, SignCount FROM authenticators WHERE User = ?")
@@ -102,34 +107,42 @@ func (s *Storage) GetAuthenticators(user webauthn.User) ([]webauthn.Authenticato
 	return authrs, nil
 }
 
+// WebAuthID is needed for webauthn protocol
 func (u *User) WebAuthID() []byte {
 	return []byte(u.Name)
 }
 
+// WebAuthName is needed for webauthn protocol
 func (u *User) WebAuthName() string {
 	return u.Name
 }
 
+// WebAuthDisplayName is needed for webauthn protocol
 func (u *User) WebAuthDisplayName() string {
 	return u.Name
 }
 
+// WebAuthID is needed for webauthn protocol
 func (a *Authenticator) WebAuthID() []byte {
 	return a.ID
 }
 
+// WebAuthCredentialID is needed for webauthn protocol
 func (a *Authenticator) WebAuthCredentialID() []byte {
 	return a.CredentialID
 }
 
+// WebAuthPublicKey is needed for webauthn protocol
 func (a *Authenticator) WebAuthPublicKey() []byte {
 	return a.PublicKey
 }
 
+// WebAuthAAGUID is needed for webauthn protocol
 func (a *Authenticator) WebAuthAAGUID() []byte {
 	return a.AAGUID
 }
 
+// WebAuthSignCount is needed for webauthn protocol
 func (a *Authenticator) WebAuthSignCount() uint32 {
 	return a.SignCount
 }
