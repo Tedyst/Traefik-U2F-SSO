bf8a5f4459aeb815f8cb8cfb747b207fd6e5cb53
Stoica <stoicatedy@gmail.com>
2019-07-31 09:26:38 +0300

    fully added sqlite

--- storage.go
+++ storage.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"log"
-	"encoding/hex"
 	"fmt"
+	"log"
+
+	"database/sql"
 
 	"github.com/koesie10/webauthn/webauthn"
 	_ "github.com/mattn/go-sqlite3"
-	"database/sql"
+)
+
+var (
+	db *sql.DB
 )
 
 //User is the user type
@@ -17,7 +21,6 @@ type User struct {
 }
 
 type Authenticator struct {
-	User         *User
 	ID           []byte
 	CredentialID []byte
 	PublicKey    []byte
@@ -30,56 +33,75 @@ type Storage struct {
 	authenticators map[string]*Authenticator
 }
 
-
 var storage = &Storage{
 	authenticators: make(map[string]*Authenticator),
 	users:          make(map[string]*User),
 }
 
 func (s *Storage) AddAuthenticator(user webauthn.User, authenticator webauthn.Authenticator) error {
-	authr := &Authenticator{
-		ID:           authenticator.WebAuthID(),
-		CredentialID: authenticator.WebAuthCredentialID(),
-		PublicKey:    authenticator.WebAuthPublicKey(),
-		AAGUID:       authenticator.WebAuthAAGUID(),
-		SignCount:    authenticator.WebAuthSignCount(),
+	stmt, err := db.Prepare("INSERT INTO authenticators(User, ID, CredentialID, PublicKey, AAGUID, SignCount) VALUES (?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
 	}
-	key := hex.EncodeToString(authr.ID)
-
-	u, ok := s.users[string(user.WebAuthID())]
-	if !ok {
-		return fmt.Errorf("user not found")
+	defer stmt.Close()
+	_, err = stmt.Exec(user.WebAuthName,
+		authenticator.WebAuthID(),
+		authenticator.WebAuthCredentialID(),
+		authenticator.WebAuthPublicKey(),
+		authenticator.WebAuthAAGUID(),
+		authenticator.WebAuthSignCount())
+	if err != nil {
+		log.Fatal(err)
+		// return fmt.Errorf("authenticator already exists")
 	}
-
-	if _, ok := s.authenticators[key]; ok {
-		return fmt.Errorf("authenticator already exists")
-	}
-
-	authr.User = u
-
-	u.Authenticators[key] = authr
-	s.authenticators[key] = authr
 
 	return nil
 }
 
 func (s *Storage) GetAuthenticator(id []byte) (webauthn.Authenticator, error) {
-	authr, ok := s.authenticators[hex.EncodeToString(id)]
-	if !ok {
-		return nil, fmt.Errorf("authenticator not found")
+	var au Authenticator
+	stmt, err := db.Prepare("SELECT ID,CredidentialID,PublicKey,AAGUID,SignCount FROM authenticators WHERE ID = ?")
+	if err != nil {
+		log.Fatal(err)
 	}
-	return authr, nil
+	rows, err := stmt.Query(id)
+	defer stmt.Close()
+	for rows.Next() {
+		err = rows.Scan(au.ID, au.CredentialID, au.PublicKey, au.AAGUID, au.SignCount)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return &au, nil
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil, fmt.Errorf("authenticator not found")
 }
 
 func (s *Storage) GetAuthenticators(user webauthn.User) ([]webauthn.Authenticator, error) {
-	u, ok := s.users[string(user.WebAuthID())]
-	if !ok {
-		return nil, fmt.Errorf("user not found")
-	}
-
 	var authrs []webauthn.Authenticator
-	for _, v := range u.Authenticators {
-		authrs = append(authrs, v)
+	stmt, err := db.Prepare("SELECT ID,CredidentialID,PublicKey,AAGUID,SignCount FROM authenticators WHERE User = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := stmt.Query(user.WebAuthName)
+	defer stmt.Close()
+	for rows.Next() {
+		var au Authenticator
+		err = rows.Scan(au.ID, au.CredentialID, au.PublicKey, au.AAGUID, au.SignCount)
+		if err != nil {
+			log.Fatal(err)
+		}
+		authrs = append(authrs, &au)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(authrs) == 0 {
+		return nil, fmt.Errorf("authenticator not found")
 	}
 	return authrs, nil
 }
@@ -116,22 +138,22 @@ func (a *Authenticator) WebAuthSignCount() uint32 {
 	return a.SignCount
 }
 
-func initStorage(){
+func initStorage() {
+	// Prepare database
 	db, err := sql.Open("sqlite3", "database")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Name           string                    `json:"name"`
-	// Authenticators map[string]*Authenticator `json:"-"`
+
 	sqlStmt := `
 	CREATE TABLE IF NOT EXISTS users (id integer not null primary key, name text);
 	CREATE TABLE IF NOT EXISTS authenticators (
-		User text,
-		AuthID BLOB,
+		User TEXT,
+		ID BLOB UNIQUE,
 		CredidentialID BLOB,
 		PublicKey BLOB,
 		AAGUID BLOB,
-		SignCount int,
+		SignCount INTEGER
 	);
 	`
 	_, err = db.Exec(sqlStmt)
@@ -139,5 +161,4 @@ func initStorage(){
 		log.Printf("%q: %s\n", err, sqlStmt)
 		return
 	}
-
 }
