96fdacd5146f5dc2813a20523301d905943bb4db
Stoica <stoicatedy@gmail.com>
2019-08-01 10:37:12 +0300

    added support for redirect after login

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -221,11 +220,22 @@ func verify(w http.ResponseWriter, r *http.Request) {
 	sess, err := sessionsstore.Get(r, "session")
 	if err != nil {
 		http.Redirect(w, r, Config.URL, http.StatusSeeOther)
+		logger.Debugw("Error getting the session",
+			"Session", sess.ID,
+		)
 		return
 	}
 
 	if sess.Values["logged"] == true {
-		fmt.Fprintf(w, "logged in.")
+		logger.Debugw("User is logged in",
+			"Session", sess.ID,
+		)
+		newURL := r.URL.Query().Get("rd")
+		if newURL != "" {
+			http.Redirect(w, r, newURL, http.StatusSeeOther)
+			return
+		}
+		http.Redirect(w, r, Config.URL, http.StatusSeeOther)
 	} else {
 		http.Redirect(w, r, Config.URL, http.StatusSeeOther)
 	}
